6c13245ba9a753af5006107f08c622fdd0165c84
Javi <javi@localhost.localdomain>
2015-06-16 16:18:42 +0200

    Moderación de comentarios

--- quiz/models/comment.js
+++ quiz/models/comment.js
@@ -1,8 +1,14 @@
 module.exports = function(sequelize, DataTypes){
 	return sequelize.define(
-		'Comment', {texto: {
+		'Comment', {
+			texto: {
 			type: DataTypes.STRING,
 			validate: {notEmpty:{msg: '-> Falta comentario'}}
-		}}
+		},
+		publicado: {
+      	type: DataTypes.BOOLEAN,
+      	defaultValue: false
+       }
+	}
 	);
 }
